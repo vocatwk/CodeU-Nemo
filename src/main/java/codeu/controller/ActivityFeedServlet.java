8f2e2e16b5e08ca28a56ae8c3a843a80c9e41f53
Guillermo M. Leal Gamboa <lealgamb@usc.edu>
2018-05-20 15:52:11 -0700

    Started to fetch user data in activity feed

--- src/main/java/codeu/controller/ActivityFeedServlet.java
+++ src/main/java/codeu/controller/ActivityFeedServlet.java
@@ -1,6 +1,11 @@
 package codeu.controller;
 
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
+
 import java.io.IOException;
+
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +15,22 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet class responsible for the activty feed page. */
 public class ActivityFeedServlet extends HttpServlet {
 
+  /** Store class that gives access to Users. */
+  private UserStore userStore;
+
   /** Set up state for handling activty feed requests. */
   @Override
   public void init() throws ServletException {
   	super.init();
+    setUserStore(UserStore.getInstance());
+  }
+
+  /**
+   * Sets the UserStore used by this servlet. This function provides a common setup method for use
+   * by the test framework or the servlet's init() function.
+   */
+  void setUserStore(UserStore userStore) {
+    this.userStore = userStore;
   }
 
   /**
@@ -23,6 +40,11 @@ public class ActivityFeedServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
+    List<User> users = userStore.getAllUsers();
+    if (users != null)
+      request.setAttribute("users", users);
+    else
+      System.out.println("USERS IS NULL");
     request.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp").forward(request, response);
   }
 
--- src/main/webapp/WEB-INF/view/activityfeed.jsp
+++ src/main/webapp/WEB-INF/view/activityfeed.jsp
@@ -1,8 +1,17 @@
+<%@ page import="java.util.List" %>
+<%@ page import="codeu.model.data.User" %>
+<%@ page import="java.util.Date" %>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Activity Feed</title>
   <link rel="stylesheet" href="/css/main.css">
+  <style>
+    #activity {
+      background-color: white;
+    }
+  </style>
 </head>
 <body>
 
@@ -22,6 +31,30 @@
     <h1>Activity</h1>
 
     <p>Here's everything that's happened on the site so far!</p>
+
+    <div id="activity">
+      
+      <%List<User> users = 
+        (List<User>) request.getAttribute("users");
+      if (users != null && !users.isEmpty()) {
+      %>
+      <ul>
+        <% 
+          for(User user : users) {
+          Date date = Date.from(user.getCreationTime());
+        %>
+        <li><b><%= date %>: </b> 
+          <a href="/profile/<%= user.getName() %>">
+          <%= user.getName() %></a> joined!</li>
+        <%
+        }
+        %>
+      </ul>
+      <%
+      }
+      %>
+    </div>
+
   </div>
   
 </body>
