318f542e4ff7e8e55a844d2991fdef0d8c6916d8
Guillermo M. Leal Gamboa <lealgamb@usc.edu>
2018-05-20 15:52:52 -0700

    Modified activity feed servlet test to test fetched user data

--- src/test/java/codeu/controller/ActivityFeedServletTest.java
+++ src/test/java/codeu/controller/ActivityFeedServletTest.java
@@ -1,7 +1,12 @@
 package codeu.controller;
 
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
 import java.io.IOException;
-
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,21 +22,32 @@ public class ActivityFeedServletTest {
   private HttpServletRequest mockRequest;
   private HttpServletResponse mockResponse;
   private RequestDispatcher mockRequestDispatcher;
+  private UserStore mockUserStore;
 
   @Before
   public void setUp() {
     activityFeedServlet = new ActivityFeedServlet();
+
     mockRequest = Mockito.mock(HttpServletRequest.class);
     mockResponse = Mockito.mock(HttpServletResponse.class);
     mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
     Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/activityfeed.jsp"))
         .thenReturn(mockRequestDispatcher);
+
+    mockUserStore = Mockito.mock(UserStore.class);
+    activityFeedServlet.setUserStore(mockUserStore);
   }
 
   @Test
   public void testDoGet() throws IOException, ServletException {
+    List<User> fakeUserList = new ArrayList<>();
+    fakeUserList.add(
+        new User(UUID.randomUUID(), "fakeUser", "fakePasswordHash", Instant.now()));
+    Mockito.when(mockUserStore.getAllUsers()).thenReturn(fakeUserList);
+
     activityFeedServlet.doGet(mockRequest, mockResponse);
 
+    Mockito.verify(mockRequest).setAttribute("users", fakeUserList);
     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
   }
 }
