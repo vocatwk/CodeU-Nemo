3e0d41ed8cb3802ad44711a148535954c8503727
jasminechau2 <jasminechau2@gmail.com>
2018-05-24 12:09:03 -0700

    adding to the user class to have an admin boolean if it adin is true go to the admin page

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -1,15 +1,14 @@
 package codeu.controller;
 
 import java.io.IOException;
-import java.time.Instant;
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
 import java.util.List;
-import java.util.UUID;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
+
 
 /** Servlet class responsible for the Admin page. */
 public class AdminServlet extends HttpServlet {
