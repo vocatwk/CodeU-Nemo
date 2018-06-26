1444257ca14d5919001dabc219068951512046ac
Guillermo M. Leal Gamboa <lealgamb@usc.edu>
2018-06-26 11:35:37 -0700

    Cleaned SearchServlet based on review comments

--- src/main/java/codeu/controller/SearchServlet.java
+++ src/main/java/codeu/controller/SearchServlet.java
@@ -1,11 +1,7 @@
 package codeu.controller;
 
 import codeu.model.data.User;
-import codeu.model.data.Conversation;
-import codeu.model.data.Message;
 import codeu.model.store.basic.UserStore;
-import codeu.model.store.basic.ConversationStore;
-import codeu.model.store.basic.MessageStore;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -20,19 +16,11 @@ public class SearchServlet extends HttpServlet {
   /** Store class that gives access to Users. */
   private UserStore userStore;
 
-  /** Store class that gives access to Conversations. */
-  private ConversationStore conversationStore;
-
-  /** Store class that gives access to Messages. */
-  private MessageStore messageStore;
-
   /** Set up state for handling search requests. */
   @Override
   public void init() throws ServletException {
     super.init();
     setUserStore(UserStore.getInstance());
-    setConversationStore(ConversationStore.getInstance());
-    setMessageStore(MessageStore.getInstance());
   }
 
   /**
@@ -44,55 +32,22 @@ public class SearchServlet extends HttpServlet {
   }
 
   /**
-   * Sets the ConversationStore used by this servlet. This function provides a 
-   * common setup method for use by the test framework or the servlet's init() function.
-   */
-  void setConversationStore(ConversationStore conversationStore) {
-    this.conversationStore = conversationStore;
-  }
-
-  /**
-   * Sets the EventStore used by this servlet. This function provides a common 
-   * setup method for use by the test framework or the servlet's init() function.
-   */
-  void setMessageStore(MessageStore messageStore) {
-    this.messageStore = messageStore;
-  }
-
-  /**
-   * TODO: Documentation
+   * This function gets the Users that contains the searchRequest (username) 
+   * and sets up the JSON object to be parsed. 
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
     String searchRequest = (String)request.getParameter("searchRequest");
 
-    // TODO 
-    if (searchRequest == null) {
-
-    }
+    // TODO: Determine what to do when searchRequest is null
     
     List<User> users = userStore.getUsers(searchRequest);
-    
     String json = new Gson().toJson(users);
     response.setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
     response.getWriter().write(json);
 
-    // List<Conversation> conversations = conversationStore.getAllConversations();
-   
-    // List<Message> messages = messageStore.getAllMessages();
-   
-    // request.getRequestDispatcher("/WEB-INF/view/search.jsp").forward(request, response);
+    // TODO Determine if we want to include Conversations and Messages
   }
-
-  /**
-  * TODO: Determine if this is needed.
-  */
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException, ServletException {
-    // TODO
-  }
-
 }
