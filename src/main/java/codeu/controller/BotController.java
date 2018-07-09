954137d78b48705f198fa065e8e09552009271eb
Guillermo M. Leal Gamboa <lealgamb@usc.edu>
2018-07-09 16:46:37 -0700

    Bot interface, former Bot is now NemoBot, BotController

---
+++ src/main/java/codeu/controller/BotController.java
@@ -0,0 +1,47 @@
+package codeu.controller;
+
+import codeu.model.data.Bot;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Controller class is responsible for controlling all Bots. 
+ * It's a singleton so all servlet classes can access the same instance.
+ */
+public class BotController {
+
+  /** Singleton instance of BotController. */
+  private static BotController instance;
+
+  /** Map that stores a mention key to it's Bot. */
+  private Map<String, Bot> botMap;
+
+  private BotController() {
+    botMap = new HashMap<String, Bot>();
+  }
+
+  /** Returns the singleton instance of BotController that should be shared between all servlet classes. */
+  public static BotController getInstance() {
+    if (instance == null) {
+      instance = new BotController();
+    }
+    return instance;
+  }
+
+  /** Adds an entry to the map. */
+  public void registerBot(String mentionKey, Bot bot) {
+    botMap.put(mentionKey, bot);
+  }
+
+  /** 
+  * Access the Bot with the given String.
+  * 
+  * @return null if the given String does not match any existing Bot. 
+  */
+  public Bot getBot(String mentionKey) {
+    if (botMap.containsKey(mentionKey)) {
+      return botMap.get(mentionKey);
+    }
+    return null;
+  }
+}
--- src/main/java/codeu/model/data/Bot.java
+++ src/main/java/codeu/model/data/Bot.java
@@ -1,49 +1,9 @@
 package codeu.model.data;
 
-import codeu.model.store.basic.UserStore;
-import java.time.Instant;
-import java.util.UUID;
-import org.mindrot.jbcrypt.BCrypt;
+/** Interface for Bot implementations. */
+public interface Bot {
 
-/** Class representing a Bot. */
-public class Bot extends User {
+  /** Creates a reply based on the keyword.*/
+  public String answerMessage(String message);
   
-  /**
-  * Constructs a new Bot.
-  */
-  public Bot() {
-    super(
-        UUID.randomUUID(), 
-        "NemoBot", 
-        BCrypt.hashpw("NemoBotPassword2018", BCrypt.gensalt()), 
-        Instant.now());
-    setAboutMe("I'm NemoBot. Want to talk to me? Simply @ mention me in any conversation!");
-    setIsAdmin(true);
-    setLastSeenNotifications(null);
-    UserStore.getInstance().addUser(this);
-  }
-
-  /*
-  * Scans a message to identify keywords.
-  */
-  public String parseMessage(String messageContent) {
-    String keyword;
-    // TODO find keyword
-
-    // If and only if no keyword was found.
-    keyword = "N/A";
-
-    return getMessageContent(keyword);
-  }
-
-  /*
-  * Creates a Message object based on the keyword.
-  */
-  private String getMessageContent(String keyword) {
-    if (!keyword.equals("N/A")) {
-      // TODO get appropriate response
-    }
-
-    return "I'm sorry. I didn't understand that.";
-  }
 }
